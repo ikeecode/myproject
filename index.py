38b417e34ceda14e853e4be89657496babbdc74a
ikeecode <ikeecode@gmail.com>
2022-04-27 13:31:52 +0000

    flaskforms in a another file

--- index.py
+++ index.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, abort
-from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, PasswordField, SubmitField, IntegerField
-from wtforms.validators import InputRequired, NumberRange
-from models.users import (User, Address, Company, Post, app, db)
+
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_bootstrap import Bootstrap
 from folium import Map, Marker
-from fromApi import FromApi as fp
 from random import randint
 from string import ascii_letters
+from fromApi import FromApi as fp
+from models.users import (User, Address, Company, Post, app, db)
+from forms import *
 
 # from models.users import app, db
 app = Flask(__name__)
@@ -37,41 +36,7 @@ def randomPassword():
 
     return password
 
-class GeneratorForm(FlaskForm):
-    numberChosen = IntegerField('', validators=[InputRequired()])
-    # generatorBtn = SubmitField('Charger des users')
 
-class UserForm(FlaskForm):
-    nameUser  = StringField('Name', validators=[InputRequired()])
-    username  = StringField('Username', validators=[InputRequired()])
-    email     = StringField('Email', validators=[InputRequired()])
-    phone     = IntegerField('Phone', validators=[InputRequired(), NumberRange(min=0, max=1_000_000)])
-    website   = StringField('Website', validators=[InputRequired()])
-    password  = PasswordField('Password', validators=[InputRequired()])
-    submit    = SubmitField('Valider les informations')
-
-class AddressForm(FlaskForm):
-    street   = StringField('Street', validators=[InputRequired()])
-    suite    = StringField('Suite', validators=[InputRequired()])
-    city     = StringField('City', validators=[InputRequired()])
-    zipcode  = StringField('Zipcode', validators=[InputRequired()])
-    lat      = FloatField('Latitude', validators=[InputRequired()])
-    lng      = FloatField('Longitude', validators=[InputRequired()])
-
-class CompanyForm(FlaskForm):
-    name        = StringField('Name ', validators=[InputRequired()])
-    catchPhrase = StringField('Catch Phrase ', validators=[InputRequired()])
-    bs          = StringField('Bs ', validators=[InputRequired()])
-
-class LoginForm(FlaskForm):
-    login = StringField('Login', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
-
-    logBtn = SubmitField('Se connecter')
-
-
-class Charger(FlaskForm):
-    charger = SubmitField('Charger')
 
 
 # la page principale
